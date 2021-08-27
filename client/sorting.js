fa3e693edf7ee1a906649b904de565f648bb4560
Nate Nelson <nathan.nelson99@gmail.com>
2021-08-27 08:25:50 -0600

    refactors selection sort

--- client/sorting.js
+++ client/sorting.js
@@ -49,19 +49,23 @@ async function bubbleSort(barsArray) {
     return barsArray
 }
 
+//Selection sort works by finding the smallest element in a given array, then moving that to the front of the array. The array is then shortened one element on the front, thereby ignoring the elements already sorted.
+
 async function selectionSort(barsArray) {
+    //Define the base index variable
     let baseIndex = 0
 
+    //Make a for loop that's equal to the length of the given array
     for (let i = 0; i < barsArray.length - 1; i++) {
+        //At the start of each loop, the base index increments up once. This is so that we can ignore the element that was sorted in the last iteration
         baseIndex = i
 
+        //Make a nested for loop that's also equal to the length of the given array. I've defined the variable here to be equal to i in the parent for loop plus one.
         for (let j = i + 1; j < barsArray.length; j++) {
+            //Here I'm checking to see if the element size at index j is less than the element size at the dynamic base index. So for example, on the first iteration through I'm checking to see if the given array at index 1 is less than the given array at index 0.
             if (parseInt(barsArray[j].getAttribute("value")) < parseInt(barsArray[baseIndex].getAttribute("value"))) {
+                //If the true, I'm setting the base index equal to j. Going back to the example I started above, the base index now equals 1.
                 baseIndex = j
-                await new Promise(resolve => setTimeout(resolve, delay))
-            }
-
-            if (baseIndex != j) {
                 await new Promise(resolve => setTimeout(resolve, delay))
             }
         }
