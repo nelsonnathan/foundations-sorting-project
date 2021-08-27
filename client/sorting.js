e9708b04c6fd64a9d0aaa6a93b3914f1677ff3cc
Nate Nelson <nathan.nelson99@gmail.com>
2021-08-27 11:22:02 -0600

    adds insertion sort functionality and removes unused buttons

--- client/sorting.js
+++ client/sorting.js
@@ -34,6 +34,8 @@ async function swapElements(barOne, barTwo) {
     await new Promise(resolve => setTimeout(resolve, delay))
 }
 
+//Bubble sort works by iterating up through an array, comparing the size of two neighboring elements. If the element size is smaller at an index higher than it's neighbor it's being compared to, then the elements are swapping positions. At the end of the first iteration, the largest element in the array will have been permanently placed at the end of the array and then ignored in the following iterations.
+
 async function bubbleSort(barsArray) {
     for (let i = 0; i < barsArray.length - 1; i++) {
         for (let j = 0; j < barsArray.length - 1 - i; j++) {
@@ -64,7 +66,7 @@ async function selectionSort(barsArray) {
         for (let j = i + 1; j < barsArray.length; j++) {
             //Here I'm checking to see if the element size at index j is less than the element size at the dynamic base index. So for example, on the first iteration through I'm checking to see if the given array at index 1 is less than the given array at index 0.
             if (parseInt(barsArray[j].getAttribute("value")) < parseInt(barsArray[baseIndex].getAttribute("value"))) {
-                //If the true, I'm setting the base index equal to j. Going back to the example I started above, the base index now equals 1.
+                //If the true, I'm setting the base index equal to j. Going back to the example I started above, the base index now equals 1 and the element size at that index is the new smallest index to compare to.
                 baseIndex = j
                 await new Promise(resolve => setTimeout(resolve, delay))
             }
@@ -76,15 +78,31 @@ async function selectionSort(barsArray) {
     }
 }
 
+//Insertion sort works by tracking two arrays - one sorted, one unsorted. It begins by adding the first element from the unsorted array, to the sorted array. It then compares the new first element in the unsorted array to the last element in the sorted array. If the new first element of the unsorted array is less than the last element in the sorted array, the last element in sorted is shifted up an index to make space for the unsorted array element to it's left.
+
+async function insertionSort(barsArray) {
+    //Here I'm starting a for loop that's equal to the length of the array. 'i' will be the first element in the unsorted array.
+    for (let i = 1; i < barsArray.length; i++) {
+        // Now I'm starting a nested for loop that will run while it's variable, 'j', is greater than zero. 'j' will essentially begin as the last element in the sorted array. if i < j, j will shift up an index and on the next iteration of this nested loop will become the second to last element in the sorted array. 'i' will continue to compare to 'j' until the if statement isn't executed and 'i' has found it's place in the sorted array.
+        for (let j = i; j > 0; j--) {
+            if (parseInt(barsArray[j].getAttribute("value")) < parseInt(barsArray[j - 1].getAttribute("value"))) {
+                await swapElements(barsArray[j], barsArray[j - 1])
+            }
+
+        }
+        await new Promise(resolve => setTimeout(resolve, delay))
+    }
+    await new Promise(resolve => setTimeout(resolve, delay))
+}
+
 let delay = 25
 
 
 
 let newChartBtn = document.getElementById("newArr")
-
 let bubbleSortBtn = document.getElementById("bubbleSort")
-
 let selectionSortBtn = document.getElementById("selectionSort")
+let insertionSortBtn = document.getElementById("insertionSort")
 
 newChartBtn.addEventListener("click", () => {
     deleteBars()
@@ -99,4 +117,9 @@ bubbleSortBtn.addEventListener("click", () => {
 selectionSortBtn.addEventListener("click", () => {
     let barsArray = document.querySelectorAll(".bars")
     selectionSort(barsArray)
+})
+
+insertionSortBtn.addEventListener("click", () => {
+    let barsArray = document.querySelectorAll(".bars")
+    insertionSort(barsArray)
 })
